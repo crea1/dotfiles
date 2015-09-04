a24a2e8ad5e8bbe19d0817a3e92f3408ceff3307
Marius Kristensen <ekst_mk@eika.no>
2015-09-04 08:26:29 +0200

    Explicitly set headers to receive github v3 response

--- scripts/bin/clone_everything_github.py
+++ scripts/bin/clone_everything_github.py
@@ -11,7 +11,8 @@ base_url = 'https://api.github.com/users/crea1/repos'
 
 # Fetch and parse JSON
 repos_url = base_url
-repos_response = requests.get(repos_url)
+headers = {'Accept': 'application/vnd.github.v3+json'}
+repos_response = requests.get(repos_url, headers=headers)
 repos_parsed_json = json.loads(repos_response.text)
 
 # Loop though repositories and clone them
