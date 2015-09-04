20ee80c002cfafb59d8a2379eb6360bb129c9f62
Marius Kristensen <ekst_mk@eika.no>
2015-09-04 08:20:44 +0200

    Add script to clone all my stuff from github :panda_face:

---
+++ scripts/bin/clone_everything_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Clones all GitHub repositories of my user to current directory.
+#
+# Import modules
+import requests
+import json
+from subprocess import call
+
+# Base URL for GitHub repo REST API for my user
+base_url = 'https://api.github.com/users/crea1/repos'
+
+# Fetch and parse JSON
+repos_url = base_url
+repos_response = requests.get(repos_url)
+repos_parsed_json = json.loads(repos_response.text)
+
+# Loop though repositories and clone them
+for repo in repos_parsed_json:
+    print "Cloning " + repo['name'] + " - \"" + repo['description'] + "\""
+    call(["git", "clone", repo['clone_url']])
+    print
