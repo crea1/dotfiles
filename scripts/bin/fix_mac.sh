18926031e02e5c46c070e84b10aad21aa772344b
Marius Kristensen <marius.kristensen@gmail.com>
2018-01-03 09:43:03 +0100

    Added script to fix mac

---
+++ scripts/bin/fix_mac.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# 
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew
+brew update
+
+# Install GNU utils
+brew tap homebrew/dupes
+brew install coreutils
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-indent --with-default-names
+brew install gnu-which --with-default-names
+brew install grep --with-default-names
+
+# Install Bash 4
+brew install bash
+
